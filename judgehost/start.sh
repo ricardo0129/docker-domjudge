cff65bbdfdf327f4f529e06be193e640825c024b
ricardo0129 <ruizr274@gmail.com>
2024-10-19 03:28:41 -0400

    bug

--- judgehost/start.sh
+++ judgehost/start.sh
@@ -6,4 +6,4 @@ WORKER_ID=$2 #number (id) of judgehost
 SERVER_URL="" #domserver url
 WORKER_PASSWORD="" #judgehost user account password in the server
 
-sudo docker run -it --privileged -v /sys/fs/cgroup:/sys/fs/cgroup --name judgehost-$(whoami)-$SERVER_ID-$WORKER_ID --hostname judgedaemon-$(whoami)-$SERVER_ID-$WORKDER_ID -e DAEMON_ID=$WORKER_ID -e DOMSERVER_BASEURL=$SERVER_URL -e JUDGEDAEMON_PASSWORD=$WORKER_PASSWORD domjudge/judgehost-extended:$DOMJUDGE_VERSION-build
+sudo docker run -it -d --privileged -v /sys/fs/cgroup:/sys/fs/cgroup --name judgehost-$(whoami)-$SERVER_ID-$WORKER_ID --hostname judgedaemon-$(whoami)-$SERVER_ID-$WORKDER_ID -e DAEMON_ID=$WORKER_ID -e DOMSERVER_BASEURL=$SERVER_URL -e JUDGEDAEMON_PASSWORD=$WORKER_PASSWORD domjudge/judgehost-extended:$DOMJUDGE_VERSION-build
